e09a1b6e826a6f89b97fa072482b22fbf7f0f032
abhijitm16 <abhijitm16@github.com>
2021-04-09 12:55:08 -0400

    Minor changes to script

--- tools/scripts/modify_image.sh
+++ tools/scripts/modify_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# != 4 ]; then
-     echo "Use ./modify_image.sh <program name> <dump dir> <path to criu modified> <full path to signal handler library"
+     echo "Use ./modify_image.sh <program name> <dump dir> <path to criu modified> <full path to signal handler library>"
 fi
 
 # Prepare the dir for process dump
@@ -12,12 +12,17 @@ rm vanilla-dump/* -rf
 sudo $3/criu/criu dump -D vanilla-dump -j -t $(pidof $1)
 sudo chown $USER:$(id -gn) vanilla-dump -R
 
+# Copy vanilla images from dump
 cp ./vanilla-dump/* .
 
+# Generate the configuration files for adding the sighandler
 $3/crit/crit config -d ./ -name "webdav" -ja 0x5797
 
+# Compile the sighandler library with configuration
 gcc -shared -fPIC multi_sig_2.c -o multi_sig_2.so
 
+# Add the sighandler into library
 $3/crit/crit ash -d . -dl $4/multi_sig_2.so -ha 0x7f0000001119 -vsa 0x7f0000000000
 
+# Restore the modified binary
 sudo $3/criu/criu restore -j -D $2
