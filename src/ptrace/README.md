3f6bf5e1dfece2d996c3ef11860c2ebba94e5299
abhijitm16 <abhijitm16@github.com>
2021-04-16 10:22:53 -0400

    cleanup of repo

--- src/ptrace/README.md
+++
@@ -1,2 +0,0 @@
-## PopSnapshot with ptrace
-Using a ptrace monitor to dump out the running process's stack live values.
--- tools/scripts/find_print_exit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
- objdump -TC /lib/x86_64-linux-gnu/libc.so.6 | grep " printf$"
- objdump -TC /lib/x86_64-linux-gnu/libc.so.6 | grep " exit$"
